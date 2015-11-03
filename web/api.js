df5804521f3f38f3389e183c989f9be48eb95255
Harry Jones <harjones@cisco.com>
2015-11-03 15:41:32 +0000

    Working server communication

--- web/api.js
+++ web/api.js
@@ -1,7 +1,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'p3plcpnl0650.prod.phx3.secureserver.net',
-  user     : 'ANEXD_ADMIN@p3plcpnl0650.prod.phx3.secureserver.net',
+  user     : 'ANEXD_ADMIN',
   password : 'gn9-MVn-6Bq-q6b',
   database : 'ANEXD'
 });
