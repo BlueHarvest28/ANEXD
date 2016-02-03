4c86bd92465edb16914a8195a2ab28ae809d8cff
BlueHarvest28 <fjharrington223@gmail.com>
2016-02-03 16:05:54 +0000

    Freds Angular Tasks
    Going to work with alex to make sure the connection between frontend
    and api are 100% working and correct

--- web/app/scripts/mobile-home.js
+++ web/app/scripts/mobile-home.js
@@ -3,16 +3,24 @@
 angular.module('ANEXD')
 .controller('MobileHomeController', [
 	'$scope',
-    function ($scope) 
+    '$http',
+    'SocketService',
+    function ($scope, $http, SocketService) 
     {			
-		$scope.anonUser = {
-			'nickname': '',
-			'email': ''
+		
+        SocketService.on('message', function (message) {
+        	console.log(message);
+        });
+        
+        $scope.anonUser = {
+			'nickname': ''
 		};
 		
 		$scope.ready = false;
     	$scope.allowNicknames = true;	
 		$scope.showLobby = false;
+        
+        var host = 'http://api-anexd.rhcloud.com/';
 		
 		$scope.goBack = function(){
 			$scope.showLobby = false;
@@ -20,16 +28,63 @@ angular.module('ANEXD')
 		};
 		
 		$scope.submitUser = function(){
-			if($scope.anonUser.nickname === ''){
-				$scope.anonUser.nickname = $scope.anonUser.email;
-			}
 			$scope.showLobby = true;
+            
+            var req = {
+                 method: 'POST',
+                 url: host + 'userReady',
+                 data: $scope.anonUser
+            };            
+            
+            $http(req).then(function successCallback(response) {
+				console.log(response);
+            }, function errorCallback(response) {
+				console.log(response);
+            }); 
 		};
 		
 		$scope.toggleReady = function(){
 			$scope.ready = !$scope.ready;
+            
+            var req = {
+                 method: 'POST',
+                 url: host + 'userReady',
+                 data: {ready: true},
+            };
+            
+            //POST For player ready
+            $http(req).then(function successCallback(response) {
+				console.log(response);
+            }, function errorCallback(response) {
+				console.log(response);
+            });        
 		};
-		
+
+/*
+        $scope.users = [{
+                'id': '',
+                'nickname': '',
+                'ready': false,             
+        }];
+
+        SocketService.on('anonUsers', function (data) {
+        
+        if($scope.showlobby == true){
+            for (var i = 0; i < data.length(); i++) {
+                var incomingId = data[i].id;
+                var incomingNickname = data[i].data.nickname;
+                var incomingReady = data[i].data.ready;
+
+                $scope.users.push({
+                    'id': incomingId,
+                    'nickname': incomingNickname,
+                    'ready': incomingReady
+                });
+            }
+            }
+        });
+*/           
+
 		$scope.users = [
 			{
 				'name': 'Edgar Badgerdon',
@@ -112,6 +167,7 @@ angular.module('ANEXD')
 				'ready': false,
 			},
 		];
+
     }
 ]);
 }());
