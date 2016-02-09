49bec153d9517cc1fcd3f37ceeaaf7e96b190b3f
M43a <Waaslam@cisco.com>
2016-02-09 17:16:00 +0000

    Revert "Shouldhide function removed for now"
    This reverts commit 85ec6bde69a7b41ce56e6d38a8bff81a0f73ca58.

--- web/app/scripts/index.js
+++ web/app/scripts/index.js
@@ -14,7 +14,7 @@ angular.module('ANEXD')
     		$scope.user = LoginService.getUser();
     	}
 
-		// $scope.shouldHide = false;
+		$scope.shouldHide = false;
         
     	$scope.login = function(email, password){
     		//Wait for the modal to animate out
@@ -37,24 +37,24 @@ angular.module('ANEXD')
 		$scope.isMobile = $rootScope.isMobile;
 	}    
 ])
-// .directive('hideOnSubmit', function(){
-// 	return{
-// 		restrict: 'A',
-// 		link: function(scope, elm, attrs) {
-// 			console.log(attrs.shouldHide);
-// 			var shouldHide = attrs.shouldHide;
-// 			if(shouldHide){
-// 				console.log('Panicccc');
-// 				return;
-// 			} else {
-// 				//in js, set variable for whether the form was successful or not 
-// 				//Put that variable into an html attribute, might have to wrap in {{variable name}}
-// 				$(elm).find('.login-submit').on('click', function() {
-// 					elm.modal('hide');
-// 				});
-// 			}
+.directive('hideOnSubmit', function(){
+	return{
+		restrict: 'A',
+		link: function(scope, elm, attrs) {
+			console.log(attrs.shouldHide);
+			var shouldHide = attrs.shouldHide;
+			if(shouldHide){
+				console.log('anus');
+				return;
+			} else {
+				//in js, set variable for whether the form was successful or not 
+				//Put that variable into an html attribute, might have to wrap in {{variable name}}
+				$(elm).find('.login-submit').on('click', function() {
+					elm.modal('hide');
+				});
+			}
 			
-// 	    }
- 	// };
-// });
+	    }
+	};
+});
 }());
