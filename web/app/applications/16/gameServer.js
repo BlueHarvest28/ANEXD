7721b8c0f6b2366c0bbc65538316a15fc7f4a9ef
BlueHarvest28 <fjharrington223@gmail.com>
2016-03-17 21:57:37 +0000

    Added Extra Code to 16 gS.js

--- web/app/applications/16/gameServer.js
+++ web/app/applications/16/gameServer.js
@@ -6,12 +6,23 @@ client.connect(1337, 'localhost', function() {
 	client.write('Connected to Server');
 });
 
-client.on('comment', function(data) {
-	console.log('Received: ' + data);
-	client.write.('comment', data);
-    client.destory();
+client.on('data', function(data) {  
+    if(data.msg.event === 'comment') { //Check the event
+        var newComment = '';
+        console.log('Received: ' + data);
+        newComment = data;
+        
+        //JSON Creation
+        var msg = {
+        'event': 'comment',
+        'data': newComment};
+        
+        //Sent to Host (player 0) via GoLang Server
+        client.write({event: 'msgplayer', player: 0, msg: {package}); 
+    }
 });
 
 client.on('close', function() {
 	console.log('Connection closed');
+    client.destroy();
 });
