afe7316f929de3eaacdc5e7e684bae891c155f5f
Harry Jones <harjones@cisco.com>
2016-03-18 00:48:44 +0000

    prep for open shift

--- web/app/applications/14/gameServer.js
+++ web/app/applications/14/gameServer.js
@@ -1,7 +1,15 @@
 var net = require('net');
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "localhost");
+
+//Launch server
+server.listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+});
+
 var client = new net.Socket();
-client.connect(1337, 'localhost', function() {
+client.connect(app.get('port') ,app.get('ip'), function() {
 	console.log('Connected');
 	client.write(new Buffer(JSON.stringify({'event': 'server'})));
 });
