26657ae6420608102fd405385578de1b76970cca
Harry Jones <harjones@cisco.com>
2016-03-31 12:51:45 +0100

    Apps linked up to corpus

--- web/app/applications/14/gameServer.js
+++ web/app/applications/14/gameServer.js
@@ -1,14 +1,21 @@
-/*
-*	TODO: 	COMMENTS
-*/
-var net = require('net');
+/**
+ * CO600 ANEXD Project Code
+ *
+ * Contributor(s): Harry Jones(HJ80)
+ * Image Annotate game server
+ * Handles instancing of the Image Annotate application
+ *
+ * Copyright (C): University Of Kent 14/03/2016 
+**/
+#!/bin/env node
+var net 	= require('net');
 var express = require('express');
 var http    = require('http');
 var app     = express();
-var server  = http.createServer(app);
 var socketio = require('socket.io-client');
 var bodyParser = require('body-parser');
 
+var serverSocket;
 var games = [];
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3004);
@@ -16,33 +23,43 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "localhost");
 app.use(bodyParser.json());
 
 //Launch server
-server.listen(app.get('port'), app.get('ip'), function () {
+app.listen(app.get('port'), app.get('ip'), function () {
     console.log("✔ Express server listening at %s:%d ", app.get('ip'), app.get('port'));
 });
 
-app.post("/",function(request, response){
-	console.log(request.body);
-	var lobbyid = request.body.lobbyid;
-	var instance = new app().init();
-	games.push(instance);
+app.post('/', function(request, response){
+	if(request.body){
+		serverSocket = socketio('http://api-anexd.rhcloud.com:8000');
+		var lobbyid = request.body.lobbyId;
+		serverSocket.on('newsession', function(data){
+			serverSocket.emit('created');
+			var i = new instance(lobbyid);
+			i.init();
+			games.push(i);	
+		});
+	}
+	response.json({});
 });
 
-function app() {
+function instance(lobbyid) {
+	var socket = socketio('http://api-anexd.rhcloud.com:8000');
     this.imageURL = '';
     this.init = function() {
-		console.log('Initialising');
-		this.socket = socketio('http://api-anexd.rhcloud.com:8000/');
-		this.socket.on('connect', function(){
-			this.socket.emit('client', 'server');
-			this.socket.emit('connectlobby', lobbyid);
-			this.socket.on('connectlobby', function(data){
-				this.run();
+		console.log('Initialising', lobbyid);
+		socket.on('connect', function(){
+			console.log('Connectiones');
+			socket.emit('client', 'server');
+			socket.emit('connectlobby', lobbyid);
+			socket.on('connectlobby', function(data){
+				if(data){
+					run();	
+				}
 			});
 		});
     };
-    this.run = function() {
+    var run = function() {
 		console.log('Running');
-        this.socket.on('new', function(data){
+        socket.on('new', function(data){
 			console.log('new');
 			if(this.imageURL){
 				console.log('sending image', this.imageURL);
@@ -50,94 +67,40 @@ function app() {
 			}
 		});
 		
-		this.socket.on('image', function(data){
+		socket.on('image', function(data){
 			console.log('image:', data);
 			this.imageURL = data;
 			var msg = {
 				'event': 'image',
 				'data': this.imageURL,
 			};
-			this.emit('msgall', {'msg': msg});
+			socket.emit('msgall', {'msg': msg});
 		});
 		
-		this.socket.on('drawing', function(data){
+		socket.on('drawing', function(data){
 			console.log('drawing:', data);
 			var msg = {
 				'event': 'drawing',
 				'data': data,
 			}
-			this.emit('msgplayer', {'player': 0, 'msg': msg});
+			socket.emit('msgplayer', {'player': 0, 'msg': msg});
 		});
 		
-		this.socket.on('save', function(data){
+		socket.on('save', function(data){
 			console.log('save:', data);
 			var msg = {
 				'event': 'save',
 				'data': data,
 			}
-			this.emit('msgplayer', {'player': 0, 'msg': msg});
+			socket.emit('msgplayer', {'player': 0, 'msg': msg});
 		});
 		
-		this.socket.on('undo', function(data){
+		socket.on('undo', function(data){
 			console.log('undo');
 			var msg = {
 				'event': 'undo',
 			}
-			this.emit('msgplayer', {'player': 0, 'msg': msg});
+			socket.emit('msgplayer', {'player': 0, 'msg': msg});
 		});	
     };
 };
-
-//
-//var play = function(namespace, instance){
-//	var socket = socketio(namespace); 
-//	games.push({'socket': socket, 'imageURL': ''});
-//	
-//	socket.on('connect', function(){
-//		console.log('game server connected to mock');
-//		
-//		socket.on('new', function(data){
-//			console.log('new');
-//			if(imageURL){
-//				console.log('sending image', imageURL);
-//				socket.emit('image', imageURL);
-//			}
-//		});
-//		
-//		socket.on('image', function(data){
-//			console.log('image:', data);
-//			imageURL = data;
-//			var msg = {
-//				'event': 'image',
-//				'data': games[instance].imageURL,
-//			};
-//			socket.emit('msgall', {'msg': msg});
-//		});
-//
-//		socket.on('drawing', function(data){
-//			console.log('drawing:', data);
-//			var msg = {
-//				'event': 'drawing',
-//				'data': data,
-//			}
-//			socket.emit('msgplayer', {'player': 0, 'msg': msg});
-//		});
-//
-//		socket.on('save', function(data){
-//			console.log('save:', data);
-//			var msg = {
-//				'event': 'save',
-//				'data': data,
-//			}
-//			socket.emit('msgplayer', {'player': 0, 'msg': msg});
-//		});
-//
-//		socket.on('undo', function(data){
-//			console.log('undo');
-//			var msg = {
-//				'event': 'undo',
-//			}
-//			socket.emit('msgplayer', {'player': 0, 'msg': msg});
-//		});	
-//	});
-//};
