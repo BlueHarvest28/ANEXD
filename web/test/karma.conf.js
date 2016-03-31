e52d4313f1c1701c72ce97074a53364b4a8d1244
Harry Jones <harjones@cisco.com>
2016-03-31 14:39:11 +0100

    *BOWER INSTALL* Removing socket-mock as it broke socket.io, expect tests to fail.

--- web/test/karma.conf.js
+++ web/test/karma.conf.js
@@ -38,7 +38,6 @@ module.exports = function(config) {
       'bower_components/angular-md5/angular-md5.js',
       'bower_components/ng-file-upload/ng-file-upload.js',
       'bower_components/ng-file-upload-shim/ng-file-upload-shim.js',
-      'bower_components/angular-socket.io-mock/angular-socket.io-mock.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
 	  'bower_components/qrcode/lib/qrcode.min.js',
